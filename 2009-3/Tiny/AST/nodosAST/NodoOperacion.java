7b834b3973203c7a4d8a3c2e5b77d8bb15b2cdcb
ziul1979 <ziul1979@gmail.com>
2010-03-09 15:59:35 +0000

    Añadida de forma conceptual la parte de construccion del AST (no probada aun), pero con algunos errores que deben ser arreglados.

--- 2009-3/Tiny/AST/nodosAST/NodoOperacion.java
+++ 2009-3/Tiny/AST/nodosAST/NodoOperacion.java
@@ -1,16 +1,18 @@
 package ast;
 
 public class NodoOperacion extends NodoBase {
+		
 	private NodoBase opIzquierdo;
 	private NodoBase opDerecho;
+	private tipoOp operacion;
 	
-	public NodoOperacion(NodoBase opIzquierdo, NodoBase opDerecho) {
+	public NodoOperacion(NodoBase opIzquierdo, tipoOp tipoOperacion, NodoBase opDerecho) {
 		super();
 		this.opIzquierdo = opIzquierdo;
 		this.opDerecho = opDerecho;
 	}
 
-	public NodoOperacion() {
+	public NodoOperacion(tipoOp tipoOperacion) {
 		super();
 		this.opIzquierdo = null;
 		this.opDerecho = null;
@@ -32,4 +34,9 @@ public class NodoOperacion extends NodoBase {
 		this.opDerecho = opDerecho;
 	}
 
+	public tipoOp getOperacion() {
+		return operacion;
+	}
+
+	
 }
